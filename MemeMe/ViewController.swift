62ff7bbe23c5cb4906f284612590e4718be98bee
Mario Cezzare <mcezzare@gmail.com>
2018-12-03 23:35:30 -0200

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Mario Cezzare on 03/12/18.
+//  Copyright © 2018 Mario Cezzare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
