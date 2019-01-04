06be9cb4935341d1de1afb68b9e62deb2eefa26a
Mario Cezzare <mcezzare@gmail.com>
2019-01-04 21:30:07 -0200

    add new controller for table view

---
+++ MemeMe/Controller/SentMemesTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Mario Cezzare on 04/01/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemesTableViewController : UITableViewController {
+    
+    // MARK: Outlets
+    @IBOutlet var sentMemesTableView: UITableView!
+    
+}
